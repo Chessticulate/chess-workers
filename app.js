f5a2a427ebead68225bba3c7c72b64c03042f1da
Brian Glaws <glaws.brian@gmail.com>
2023-09-21 20:45:12 -0400

    progress on app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const chess = require('ShallowPink');
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +12,19 @@ app.use(express.json());
 app.get('/info', (request, response) => {
     response.status(200).json({"message": "hello world"});
 });
+
+app.post('/move', (request, response) => {
+    if (request.body.fen === undefined) {
+        response.status(400).json({"message": "FEN string missing from request body"});
+        return;
+    }     
+    if (request.body.move === undefined) {
+        response.status(400).json({"message": "move string missing from request body"});
+        return;
+    }
+    // if (request.body.move)
+    // const chess = new Chess();
+        
+}
 
 module.exports = app;
