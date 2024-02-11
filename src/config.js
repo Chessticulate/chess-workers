a1f7a21aa51920c0e592d9ab1174573535e0b6de
Kyle Glaws <kyle.glaws@gmail.com>
2024-02-10 20:35:04 -0500

    fix default port config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const SERVER_PORT = process.env.PORT || 3000;
 
 const AI_DEPTH = process.env.AI_DEPTH || 3;
 
 const N_WORKERS = process.env.N_WORKERS;
 
-module.exports = { PORT, AI_DEPTH, N_WORKERS };
+module.exports = { SERVER_PORT, AI_DEPTH, N_WORKERS };
 
